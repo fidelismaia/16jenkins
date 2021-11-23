72db4bcc563c3e7a289a5c296d3fc5edefbb7951
Lucas Fidelis Maia <85111490+fidelismaia@users.noreply.github.com>
2021-11-23 09:33:55 -0300

    1

---
+++ src/test/java/b/ctd/lucas/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package b.ctd.lucas.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
